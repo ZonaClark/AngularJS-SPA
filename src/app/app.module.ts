995c6a2e47ddedf4ecb84aa8648b15c8c8881d32
ZonaClark <zona.clark56@gmail.com>
2019-03-12 09:08:24 -0400

    property and event binding of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { UserComponent } from './user.component'
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   template: `
-    <input type="text" [(ngModel)]="name">
+    <input type="text" (input)="onUserInput($event)" [value]="name">
     <p>Hello {{ name }}!</p>
     <p>User Component</p>
   `
 })
 export class UserComponent {
-  name = "max";
+  @Input() name;
+  @Output() nameChanged = new EventEmitter<string>();
 
   onUserInput(event) {
-    this.name = event.target.value;
+    this.nameChanged.emit(event.target.value);
   }
 }
