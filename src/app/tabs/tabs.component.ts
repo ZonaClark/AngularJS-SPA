caffdfddbc1c3ac270bf32452a7a4d858bb98ceb
ZonaClark <zona.clark56@gmail.com>
2019-03-13 08:28:25 -0400

    pass data via property binding

---
+++ src/app/tabs/tabs.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.css']
+})
+export class TabsComponent implements OnInit {
+  todos = [
+    { todo: 'learn Angular', category: '' },
+    { todo: 'do laundry', category: '' }
+  ];
+  choosenCategory = 'all';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick(todoItemCategory) {
+    this.choosenCategory = todoItemCategory;
+  }
+
+  getTodos() {
+    if (this.choosenCategory === 'all') {
+      return this.todos.slice();
+    }
+    return this.todos.filter((todo) => {
+      return todo.category === this.choosenCategory;
+    });
+  }
+}
